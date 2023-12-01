9c490cad6c6fa5bce5a960a36a7ae4ca035e2bc7
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2023-12-01 18:54:51 +0200

    ^ update returnError

--- scripts/returnError.ts
+++ scripts/returnError.ts
@@ -1,23 +1,38 @@
-// wrapperCount - a parameter that sets the number of wrappers around the function to shift the error array
-export function returnError(
-	error: unknown,
-	appName?: string,
-	wrapperCount = 0,
-	deep = Infinity,
-): string {
-	const errorCount = 2 + wrapperCount;
+export interface ReturnError {
+   status: number;
+   message: string;
+}
 
-	let message = 'Sorry, something went wrong ¯\\_(ツ)_/¯!';
-	if (error) {
-		if (typeof error === 'string') message = error;
-		if (error instanceof Error) message = error.message;
-	}
-	if (appName) {
-		// eslint-disable-next-line no-console
-		console.error(`${appName} >`, `${message}\n`, new Error().stack?.split('\n').splice(errorCount, deep).join('\n'));
-	} else {
-		// eslint-disable-next-line no-console
-		console.error(`${message}\n`, new Error().stack?.split('\n').splice(errorCount, deep).join('\n'));
-	}
-	return message;
+//* WrapperCount - a parameter that sets the number of wrappers around the function to shift the error array
+export function returnError(
+   error: unknown,
+   status?: number,
+   appName?: string,
+   wrapperCount = 0,
+   deep = Infinity,
+): ReturnError {
+   const errorCount = 2 + wrapperCount;
+
+   status = status || -1;
+
+   let message = 'Sorry, something went wrong ¯\\_(ツ)_/¯!';
+   if (error) {
+      if (typeof error === 'string') message = error;
+      if (error instanceof Error) {
+         message = error.message;
+      }
+   }
+   if (appName) {
+      console.error(
+         `${appName} >`,
+         `${status} | ${message}\n`,
+         new Error().stack?.split('\n').splice(errorCount, deep).join('\n'),
+      );
+   } else {
+      console.error(
+         `${status} | ${message}\n`,
+         new Error().stack?.split('\n').splice(errorCount, deep).join('\n'),
+      );
+   }
+   return { status, message };
 }
